251b2d955f054b794ee48dc85c8ec055114544d9
NJC <nicholasjconn@gmail.com>
2014-11-12 21:01:19 -0500

    Added sudo to everything.

--- SetupAMI.sh
+++ SetupAMI.sh
@@ -2,20 +2,20 @@
 sudo apt-get update
 sudo apt-get install python3-pip
 
-apt-get install liblapack-dev
-apt-get install gfortran
+sudo apt-get install liblapack-dev
+sudo apt-get install gfortran
 
-pip3 install -U setuptools
-pip3 install -U pip
+sudo pip3 install -U setuptools
+sudo pip3 install -U pip
 
 export LAPACK=/usr
 export ATLAS=/usr/lib/libatlas.so
 export BLAS=/usr/lib/libblas.so
-pip3 install numpy
-pip3 install scipy
+sudo pip3 install numpy
+sudo pip3 install scipy
  
-pip3 install ipython[all]
-pip3 install cython
-pip3 install greenlet
-pip3 install scoop
-pip3 install deap
+sudo pip3 install ipython[all]
+sudo pip3 install cython
+sudo pip3 install greenlet
+sudo pip3 install scoop
+sudo pip3 install deap
